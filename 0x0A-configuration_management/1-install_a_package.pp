62b77872fdacd20822a6eba12ffe7b9f1b1f1b81
kerubo <48017280+aragakerubo@users.noreply.github.com>
2024-04-20 17:06:41 -0400

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,6 @@
 # Installs flask from pip3
 
-package { 'flask':
-  ensure   => '2.1.0',
-  name     => 'flask',
-  provider => 'pip3'
+package { 'Flask':
+    ensure   => '2.1.0',
+    provider => 'pip3',
 }
