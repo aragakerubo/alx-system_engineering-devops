a78246effc4e0e05363c62843be1f3800f453bc6
aragakerubo <aragakerubo@gmail.com>
2024-04-29 10:04:09 -0700

    2. Add a custom HTTP header with Puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,23 +1,9 @@
-# Just as in task #0, we’d like you to automate the task of creating a custom HTTP header response, but with Puppet.
+# automate the task of creating a custom HTTP header response, but with Puppet
 
-# The name of the custom HTTP header must be X-Served-By
-# The value of the custom HTTP header must be the hostname of the server Nginx is running on
-# Write 2-puppet_custom_http_response_header.pp so that it configures a brand new Ubuntu machine to the requirements asked in this task
-
-# Install the package nginx
-package { 'nginx':
-  ensure => 'installed',
-}
-
-# Create a custom HTTP header response
 exec { 'command':
-  command => 'printf "server {\nlisten 80 default_server;\nlisten [::]:80 default_server;\nserver_name _;\nroot /var/www/html;\nadd_header X-Served-By $HOSTNAME;\nindex index.html index.htm index.nginx-debian.html;\nlocation / {\ntry_files $uri $uri/ =404;\n}\n}" > /etc/nginx/sites-available/default',
-  provider => 'shell',
-}
-
-# Restart the Nginx service
-service { 'nginx':
-  ensure => 'running',
-  enable => 'true',
-  require => Exec['command'],
+  command  => 'apt-get -y update;
+  apt-get -y install nginx;
+  sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
+  service nginx restart',
+  provider => shell,
 }
