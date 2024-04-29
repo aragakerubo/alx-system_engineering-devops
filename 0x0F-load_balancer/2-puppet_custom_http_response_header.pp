2b1005362f81a01fead8cf33bc13a8863ff2d1d9
aragakerubo <aragakerubo@gmail.com>
2024-04-29 10:02:44 -0700

    2. Add a custom HTTP header with Puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,41 +1,23 @@
-# Install nginx package
+# Just as in task #0, we’d like you to automate the task of creating a custom HTTP header response, but with Puppet.
+
+# The name of the custom HTTP header must be X-Served-By
+# The value of the custom HTTP header must be the hostname of the server Nginx is running on
+# Write 2-puppet_custom_http_response_header.pp so that it configures a brand new Ubuntu machine to the requirements asked in this task
+
+# Install the package nginx
 package { 'nginx':
-  ensure => installed,
+  ensure => 'installed',
 }
 
-# Define custom HTTP header configuration
-file { '/etc/nginx/sites-available/default':
-  ensure  => file,
-  content => "
-server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-
-    root /var/www/html;
-    index index.html index.htm index.nginx-debian.html;
-
-    server_name _;
-
-    location / {
-        try_files \$uri \$uri/ =404;
-    }
-
-    location /redirect_me {
-        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
-    }
-
-    # Custom HTTP header
-    add_header X-Served-By \$hostname;
-}
-  ",
-  require => Package['nginx'],
+# Create a custom HTTP header response
+exec { 'command':
+  command => 'printf "server {\nlisten 80 default_server;\nlisten [::]:80 default_server;\nserver_name _;\nroot /var/www/html;\nadd_header X-Served-By $HOSTNAME;\nindex index.html index.htm index.nginx-debian.html;\nlocation / {\ntry_files $uri $uri/ =404;\n}\n}" > /etc/nginx/sites-available/default',
+  provider => 'shell',
 }
 
-# Create a symbolic link to enable the site
-file { '/etc/nginx/sites-enabled/default':
-  ensure => link,
-  target => '/etc/nginx/sites-available/default',
-  force  => true,
-  notify => Service['nginx'],
+# Restart the Nginx service
+service { 'nginx':
+  ensure => 'running',
+  enable => 'true',
+  require => Exec['command'],
 }
-
