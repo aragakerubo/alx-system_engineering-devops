733485440febdc4139c6495df28c8e07607878b2
aragakerubo <aragakerubo@gmail.com>
2024-06-05 01:44:00 +0000

    0x16. API advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,14 +5,17 @@ for a given subreddit"""
 import requests
 
 
+
 def number_of_subscribers(subreddit):
-    """Function that queries the Reddit API and returns the number of
-    subscribers for a given subreddit"""
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent': 'My User Agent 1.0'}
-    response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-        data = response.json()
-        return data.get('data').get('subscribers')
-    else:
+    """Gets the number of subscribers for a given subreddit"""
+    url = "http://reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
         return 0
+    dic = response.json()
+    if "data" not in dic:
+        return 0
+    if "subscribers"not in dic.get("data"):
+        return 0
+    return response.json()["data"]["subscribers"]
