704756d3269eec0993fe703511fb7f18e09d4f63
aragakerubo <aragakerubo@gmail.com>
2024-06-06 06:24:34 +0000

    0. How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,9 +5,7 @@ import requests
 
 def number_of_subscribers(subreddit):
     headers = {
-        "User-Agent": "Mozilla/10.0 (Windows NT 10.0; Win64; x64) \
-                AppleWebKit/537.36 (KHTML, like Gecko) \
-                Chrome/91.0.4472.124 Safari/537.36"
+        "User-Agent": "Linux: 0x16. API advanced v1.0 by u/devara"
     }
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
 
